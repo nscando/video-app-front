d7a828d836eb38fed7f246be648fdcb87ce3f17d
nscando <nicolas.scandizzo@gmail.com>
2020-09-12 13:23:03 -0300

    UI validations

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { loginRequest } from '../actions';
@@ -11,8 +11,8 @@ import '../assets/styles/components/Login.scss';
 
 
 const Login = props => {
-     const [form, setValues]= useState({
-          email:'',
+     const [form, setValues] = useState({
+          email: '',
      })
 
      const handleInput = event => {
@@ -21,61 +21,66 @@ const Login = props => {
                [event.target.name]: event.target.value
           })
      }
-     
+
      const handleSubmit = event => {
           event.preventDefault();
           props.loginRequest(form);
           props.history.push('/');
-          
+
      }
 
      return (
-     <>
-     <Header isLogin/>
-     <section className="login">
-          <section className="login__container">
-               <h2>Inicia sesión</h2>
-               <form className="login__container--form" onSubmit={handleSubmit}>
-                    <input 
-                    name="email"
-                    className="input" 
-                    type="text" 
-                    placeholder="Correo" 
-                    onChange={handleInput}
-                    />     
-                    
-                    <input 
-                    name="password"
-                    className="input" 
-                    type="password" 
-                    placeholder="Contraseña" 
-                    onChange={handleInput}
-                    />    
-                    
-                    <button className="button">Iniciar sesión</button>
-                    <div className="login__container--remember-me">
-                         <label>
-                              <input type="checkbox" id="cbox1" value="first_checkbox" />Recuérdame
-                              </label>
-                         <a href="/">Olvidé mi contraseña</a>
-                    </div>
-               </form>
-               <section className="login__container--social-media">
-                    <div><img src={googleIcon} /> Inicia sesión con Google</div>
-                    <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
-               </section>
-               <p className="login__container--register">
-                    No tienes ninguna cuenta
-               
-               <Link to="/register">
-                         Regístrate
-               </Link>
-               </p>
-          </section>
-     </section>
-     </>
+          <>
+               <Header isLogin />
+               <section className="login">
+                    <section className="login__container">
+                         <h2>Inicia sesión</h2>
+                         <form className="login__container--form" onSubmit={handleSubmit}>
+                              <input
+                                   name="email"
+                                   className="input"
+                                   type="text"
+                                   placeholder="Correo"
+                                   onChange={handleInput}
+                              />
 
-)
+                              <input
+                                   name="password"
+                                   className="input"
+                                   type="password"
+                                   placeholder="Contraseña"
+                                   onChange={handleInput}
+                              />
+
+                              <button className="button">Iniciar sesión</button>
+                              <div className="login__container--remember-me">
+                                   <label>
+                                        <input type="checkbox" id="cbox1" value="first_checkbox" />Recuérdame {' '}
+
+                                   </label>
+                              </div>
+                              <div className="login__container--remember-me">
+                                   <label>
+                                        <a href="/">Olvidé mi contraseña</a>
+                                   </label>
+                              </div>
+                         </form>
+                         <section className="login__container--social-media">
+                              <div><img src={googleIcon} /> Inicia sesión con Google</div>
+                              <div><img src={twitterIcon} /> Inicia sesión con Twitter</div>
+                         </section>
+                         <p className="login__container--register">
+                              No tienes ninguna cuenta {' '}
+
+                              <Link to="/register">
+                                   Regístrate
+               </Link>
+                         </p>
+                    </section>
+               </section>
+          </>
+
+     )
 }
 
 const mapDistpatchToProps = {
